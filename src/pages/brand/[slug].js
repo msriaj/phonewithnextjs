84df8584024a0930d698b5e4f3ae191154a44220
MD RIAJ <contact.riaj@gmail.com>
2023-03-28 18:31:08 +0600

    aa

--- src/pages/brand/[slug].js
+++ src/pages/brand/[slug].js
@@ -15,6 +15,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Brand({ phoneList, categoryInfo, totalPage }) {
   const Router = useRouter();
@@ -22,6 +23,34 @@ export default function Brand({ phoneList, categoryInfo, totalPage }) {
   const { Panel } = Collapse;
   const page = query.page || 1;
 
+  const [infoIDs, setInfoIDs] = useState([]);
+  const [overviewIDs, setOverviewIDs] = useState([]);
+
+  const infoIDHandaler = (value) => {
+    if (infoIDs.includes(value)) {
+      setInfoIDs(infoIDs.filter((id) => id !== value));
+    } else {
+      setInfoIDs([...infoIDs, value]);
+    }
+  };
+  const overviewIDHandaler = (value) => {
+    if (overviewIDs.includes(value)) {
+      setOverviewIDs(overviewIDs.filter((id) => id !== value));
+    } else {
+      setOverviewIDs([...overviewIDs, value]);
+    }
+  };
+
+  if (infoIDs.length > 0 || overviewIDs.length > 0) {
+    Router.push({
+      pathname: Router.pathname,
+      query: {
+        ...query,
+        information: infoIDs.join(","),
+        overview: overviewIDs.join(","),
+      },
+    });
+  }
   return (
     <>
       <Head>
@@ -84,13 +113,50 @@ export default function Brand({ phoneList, categoryInfo, totalPage }) {
               </Panel>
             </Collapse>{" "}
           </div>
-          {Object.keys(filterOptions).map((key) => (
-            <div key={key} className="bg-white rounded-[4px] shadow-sm">
+          {Object.keys(information).map((key) => (
+            <div
+              key={information[key]}
+              className="bg-white rounded-[4px] shadow-sm"
+            >
               <Collapse expandIconPosition={"end"} bordered={false}>
                 <Panel header={key} key="1">
-                  {filterOptions[key].map((option) => (
-                    <div className="flex p-2 hover:bg-gray-100" key={option}>
-                      <Checkbox>{option}</Checkbox>
+                  {information[key].map((option) => (
+                    <div
+                      className="flex p-2 hover:bg-gray-100"
+                      key={Object.values(option)[0]}
+                    >
+                      <Checkbox
+                        name="information"
+                        onChange={(e) => infoIDHandaler(e.target.value)}
+                        value={Object.keys(option)[0]}
+                      >
+                        {Object.values(option)[0]}
+                      </Checkbox>
+                    </div>
+                  ))}
+                </Panel>
+              </Collapse>
+            </div>
+          ))}
+          {Object.keys(overview).map((key) => (
+            <div
+              key={overview[key]}
+              className="bg-white rounded-[4px] shadow-sm"
+            >
+              <Collapse expandIconPosition={"end"} bordered={false}>
+                <Panel header={key} key="1">
+                  {overview[key].map((option) => (
+                    <div
+                      className="flex p-2 hover:bg-gray-100"
+                      key={Object.values(option)[0]}
+                    >
+                      <Checkbox
+                        name="overview"
+                        onChange={(e) => overviewIDHandaler(e.target.value)}
+                        value={Object.keys(option)[0]}
+                      >
+                        {Object.values(option)[0]}
+                      </Checkbox>
                     </div>
                   ))}
                 </Panel>
@@ -178,48 +244,130 @@ export async function getServerSideProps({ params, query }) {
   };
 }
 
-const filterOptions = {
-  Brands: [
-    "Apple",
-    "Samsung",
-    "One Plus",
-    "Realme",
-    "Redmi",
-    "Vivo",
-    "Oppo",
-    "Nokia",
-    "Honor",
-    "Asus",
-    "Lenovo",
+const information = {
+  "Display Size": [
+    { 2001: "5.5 inch" },
+    { 2002: "5.6 inch" },
+    { 2003: "5.7 inch" },
+    { 2004: "5.8 inch" },
+    { 2005: "5.9 inch" },
+    { 2006: "6.0 inch" },
+    { 2007: "6.1 inch" },
+    { 2008: "6.2 inch" },
+    { 2009: "6.3 inch" },
+    { 2010: "6.4 inch" },
+    { 2011: "6.5 inch" },
+    { 2012: "6.6 inch" },
+    { 2013: "6.7 inch" },
+    { 2014: "6.8 inch" },
+    { 2015: "6.9 inch" },
   ],
-  Availability: ["In Stock", "Out of Stock", "Up Coming"],
-  "Display Size": ["5.5", "5.7", "5.8", "6.0", "6.1", "6.2", "6.3", "6.4"],
-  "Display Resolution": ["720 x 1280", "1080 x 1920", "1440 x 2560"],
-  "Display Type": ["IPS LCD", "AMOLED", "OLED"],
-  "Display Protection": ["Corning Gorilla Glass", "Asahi Dragontrail Glass"],
-  OS: ["Android", "iOS"],
-  Processor: ["Qualcomm Snapdragon", "Apple A", "MediaTek", "Exynos"],
-  Ram: ["2 GB", "3 GB", "4 GB", "6 GB", "8 GB"],
-  "Internal Storage": ["16 GB", "32 GB", "64 GB", "128 GB", "256 GB", "512 GB"],
-  "Primary Camera": ["8 MP", "12 MP", "16 MP", "20 MP", "24 MP", "32 MP"],
+  "Display Resolution": [
+    { 1001: "720 x 1280" },
+    { 1002: "1080 x 1920" },
+    { 1003: "1440 x 2560" },
+    { 1004: "2160 x 3840" },
+  ],
+  "Display Type": [
+    { 3001: "IPS LCD" },
+    { 3002: "AMOLED" },
+    { 3003: "OLED" },
+    { 3004: "Super AMOLED" },
+    { 3005: "Super AMOLED Plus" },
+  ],
+  Chipset: [
+    { 4001: "Exynos" },
+    { 4002: "Snapdragon" },
+    { 4003: "Kirin" },
+    { 4004: "MediaTek" },
+    { 4005: "Apple A" },
+    { 4006: "Qualcomm" },
+    { 4007: "HiSilicon" },
+    { 4008: "Spreadtrum" },
+    { 4009: "Rockchip" },
+    { 4010: "Nvidia" },
+    { 4011: "Intel" },
+  ],
+  OS: [
+    {
+      5001: "Android",
+    },
+    {
+      5002: "iOS",
+    },
+    {
+      5003: "Windows",
+    },
+    {
+      5004: "BlackBerry",
+    },
+  ],
+  Storage: [
+    { 100002: "16GB" },
+    { 100003: "32GB" },
+    { 100004: "64GB" },
+    { 100005: "128GB" },
+    { 100006: "256GB" },
+    { 100007: "512GB" },
+    { 100008: "1TB" },
+  ],
+
   Features: [
-    "3G",
-    "4G",
-    "5G",
-    "Dual SIM",
-    "Fingerprint Sensor",
-    "Face Unlock",
-    "NFC",
-    "USB Type-C",
-    "Wireless Charging",
-    "Fast Charging",
-    "Water Resistant",
-    "Dust Resistant",
-    "IP68",
-    "IP69",
-    "IP69K",
-    "IP68",
-    "IP69",
-    "IP69K",
+    { 8001: "Accelerometer" },
+    { 8002: "Ambient light sensor" },
+    { 8003: "Barometer" },
+    { 8004: "Compass" },
+    { 8005: "Fingerprint (front-mounted)" },
+    { 8006: "Fingerprint (rear-mounted)" },
+    { 8007: "Gyroscope" },
+    { 8008: "Heart rate" },
+    { 8009: "Proximity sensor" },
+    { 8010: "SpO2" },
+    { 8011: "Temperature sensor" },
+    { 8012: "USB OTG" },
+    { 8013: "Face ID" },
+    { 8014: "NFC" },
+    { 8015: "Wireless charging" },
+    { 8016: "Fast charging" },
+    { 8017: "Qi wireless charging" },
+    { 8018: "Reverse wireless charging" },
+    { 8019: "Fast wireless charging" },
+  ],
+};
+
+const overview = {
+  "RAM Size": [
+    { 6003: "2GB" },
+    { 6005: "3GB" },
+    { 6007: "4GB" },
+    { 6009: "6GB" },
+    { 6011: "8GB" },
+    { 6013: "12GB" },
+    { 6015: "16GB" },
+  ],
+  Camera: [
+    { 7001: "8MP" },
+    { 7002: "12MP" },
+    { 7003: "16MP" },
+    { 7004: "20MP" },
+    { 7005: "24MP" },
+    { 7006: "32MP" },
+    { 7007: "48MP" },
+    { 7008: "64MP" },
+    { 7009: "108MP" },
+  ],
+
+  "Battery Capacity": [
+    { 9001: "1000mAh" },
+    { 9002: "1500mAh" },
+    { 9003: "2000mAh" },
+    { 9004: "2500mAh" },
+    { 9005: "3000mAh" },
+    { 9006: "3500mAh" },
+    { 9007: "4000mAh" },
+    { 9008: "4500mAh" },
+    { 9009: "5000mAh" },
+    { 9010: "5500mAh" },
+    { 9011: "6000mAh" },
   ],
 };
