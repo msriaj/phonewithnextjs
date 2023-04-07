ced63876ce2ea469f1214a49e0f4dd28af7374f7
MD RIAJ <contact.riaj@gmail.com>
2023-04-08 04:43:54 +0600

    new design 2

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-//export const API = "https://mobilepricebd.tech/api";
-export const API = "http://localhost:5000/api";
+export const API = "https://mobilepricebd.tech/api";
+// export const API = "http://localhost:5000/api";
 export const APP_NAME = "MobilePriceBD";
