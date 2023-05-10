77399ae42438b977a9d8d82caca1e3310c70a768
Yeasin Jabed <jabed50000@gmail.com>
2023-05-10 23:31:02 +0600

    google meta tag has been added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document({}) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="YIQekWCNj26kiwJ1cjV3nnKvuRxzQnrM4IA4BO4N9Lw"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
